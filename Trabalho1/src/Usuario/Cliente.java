ba3dbe78c09d1fd5d02e3ed825ff7d30096f93b1
Willian Murayama <willianyos13@gmail.com>
2022-10-25 00:25:19 -0300

    Criação das subclasses de usuário

---
+++ Trabalho1/src/Usuario/Cliente.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Usuario;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Willian
+ */
+public class Cliente extends Usuario {
+
+    protected LocalDate dataCadastro;
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+}
---
+++ Trabalho1/src/Usuario/Corretor.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Usuario;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Willian
+ */
+public class Corretor extends Usuario {
+
+    protected String creci;
+    protected float salario;
+    protected String pis;
+    protected LocalDate dataAdmissao;
+
+    public String getCreci() {
+        return creci;
+    }
+
+    public void setCreci(String creci) {
+        this.creci = creci;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public String getPis() {
+        return pis;
+    }
+
+    public void setPis(String pis) {
+        this.pis = pis;
+    }
+
+    public LocalDate getDataAdmissao() {
+        return dataAdmissao;
+    }
+
+    public void setDataAdmissao(LocalDate dataAdmissao) {
+        this.dataAdmissao = dataAdmissao;
+    }
+
+}

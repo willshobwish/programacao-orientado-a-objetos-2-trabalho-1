2fc40694290f76614b4c4c39ff53962862b09b46
Willian Murayama <willianyos13@gmail.com>
2022-10-25 17:02:08 -0300

    Implementação dos atributos

--- Trabalho1/src/Unicos/Aluguel.java
+++ Trabalho1/src/Unicos/Aluguel.java
@@ -4,10 +4,21 @@
  */
 package Unicos;
 
-/**
- *
- * @author Willian
- */
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Aluguel {
+
+    private int codigoAluguel;
+    private Cliente cliente;
+    private Corretor corretor;
+    private Imovel imovel;
+    private LocalDate dataAluguel;
+    private LocalDate dataDevolucao;
+    private LocalDate dataPagamentoMensal;
+    private float valorTotalAluguel;
+    private Pagamento formaPagamento;
+    private ArrayList<Seguro> segurosContratados;
+    private boolean pago;
 
 }

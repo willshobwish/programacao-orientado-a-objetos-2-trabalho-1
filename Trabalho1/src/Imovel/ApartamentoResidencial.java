bbe6a485262f96b0c4f12e1f742060476e065e1c
Willian Murayama <willianyos13@gmail.com>
2022-10-25 20:27:26 -0300

    Implementação de classes
    Classes implementadas:
    Imovel
    ApartamentoResidencial
    CasaResidencial
    Comercial

---
+++ Trabalho1/src/Imovel/ApartamentoResidencial.java
@@ -0,0 +1,46 @@
+/*
+ * Trabalho de programacao orientado a objetos II
+ */
+package Imovel;
+
+import java.time.LocalDate;
+
+/*
+ *
+ * Bruno Augusto Furquim
+ * Giovanna Silva Custodio
+ * Willian Yoshio Murayama
+ *
+ */
+public class ApartamentoResidencial extends Imovel {
+
+    protected int andar;
+    protected float valorCondominio;
+
+    public ApartamentoResidencial(int andar, float valorCondominio, int codigoImovel, String endereco, LocalDate dataConstrucao, float areaTotal, float areaConstruida, int qtdDormitorios, int qtdBanheiros, int qtdVagasGaragem, float valorIPTU, float valorVenda, float valorAluguel) {
+        super(codigoImovel, endereco, dataConstrucao, areaTotal, areaConstruida, qtdDormitorios, qtdBanheiros, qtdVagasGaragem, valorIPTU, valorVenda, valorAluguel);
+        this.andar = andar;
+        this.valorCondominio = valorCondominio;
+    }
+
+    public float getValorAluguel() {
+        return valorAluguel += valorCondominio;
+    }
+
+    public int getAndar() {
+        return andar;
+    }
+
+    public void setAndar(int andar) {
+        this.andar = andar;
+    }
+
+    public float getValorCondominio() {
+        return valorCondominio;
+    }
+
+    public void setValorCondominio(float valorCondominio) {
+        this.valorCondominio = valorCondominio;
+    }
+
+}

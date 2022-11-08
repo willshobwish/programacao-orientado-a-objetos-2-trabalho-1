3e85e2f2aa708da167ad6c8a2088cef9b7b75f8d
gio-custodio <73026857+gio-custodio@users.noreply.github.com>
2022-11-08 13:34:45 -0300

    Update Cartao.java
    Criei cartão

--- Trabalho1/src/Pagamento/Cartao.java
+++ Trabalho1/src/Pagamento/Cartao.java
@@ -8,6 +8,48 @@ package Pagamento;
  *
  * @author Willian
  */
-public class Cartao {
 
+public class Cartao extends Pagamento{
+    private String nome;
+    private String bandeira;
+    private String numero;
+    
+    public Cartao(String nome, String bandeira, String numero){
+        super("Cartão");
+        this.nome = nome;
+        this.bandeira = bandeira;
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getBandeira() {
+        return bandeira;
+    }
+
+    public void setBandeira(String bandeira) {
+        this.bandeira = bandeira;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    
+    @Override
+    public String toString(){
+        return "\nTipo de Pagamento: " + getTipoPagamento()
+                + "\nNome: " + getNome()
+                + "\nBandeira: " + getBandeira()
+                + "\nNúmero: " + getNumero();
+    }
 }

131c976fbe6c49b3a4fc30c951d47c5ee24085ea
Willian Murayama <willianyos13@gmail.com>
2022-11-13 01:10:52 -0300

    Criação da classe serialização
    A intenção é realocar as serializações

---
+++ Trabalho1/src/Serializacao/Serializacao.java
@@ -0,0 +1,16 @@
+/*
+ * Trabalho de programacao orientado a objetos II
+ */
+
+package Serializacao;
+
+/*
+ *
+ * Bruno Augusto Furquim
+ * Giovanna Silva Custodio
+ * Willian Yoshio Murayama
+ * 
+ */
+public class Serializacao {
+
+}

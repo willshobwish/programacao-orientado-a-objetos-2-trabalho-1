242309e223452ef2b918bcf62cfac246bf967111
Willian Murayama <willianyos13@gmail.com>
2022-11-13 23:05:24 -0300

    Modificação do tipo de atributo
    Modificação para final

--- Trabalho1/src/Unicos/Configuracao.java
+++ Trabalho1/src/Unicos/Configuracao.java
@@ -13,11 +13,11 @@ package Unicos;
 public final class Configuracao {
 
 //    private static Configuracao instanciaConfiguracao = null;
-    private String arquivoAlugueis = "src\\Arquivos\\alugueis.ser";
-    private String arquivoVendas = "src\\Arquivos\\vendas.ser";
-    private String arquivoImoveis = "src\\Arquivos\\imoveis.ser";
-    private String arquivoUsuarios = "src\\Arquivos\\usuarios.ser";
-    private String arquivoSeguros = "src\\Arquivos\\seguros.ser";
+    private final String arquivoAlugueis = "src\\Arquivos\\alugueis.ser";
+    private final String arquivoVendas = "src\\Arquivos\\vendas.ser";
+    private final String arquivoImoveis = "src\\Arquivos\\imoveis.ser";
+    private final String arquivoUsuarios = "src\\Arquivos\\usuarios.ser";
+    private final String arquivoSeguros = "src\\Arquivos\\seguros.ser";
 
     public Configuracao() {
     }
@@ -26,40 +26,19 @@ public final class Configuracao {
         return arquivoAlugueis;
     }
 
-    public void setArquivoAlugueis(String arquivoAlugueis) {
-        this.arquivoAlugueis = arquivoAlugueis;
-    }
-
     public String getArquivoVendas() {
         return arquivoVendas;
-    }
-
-    public void setArquivoVendas(String arquivoVendas) {
-        this.arquivoVendas = arquivoVendas;
     }
 
     public String getArquivoImoveis() {
         return arquivoImoveis;
     }
 
-    public void setArquivoImoveis(String arquivoImoveis) {
-        this.arquivoImoveis = arquivoImoveis;
-    }
-
     public String getArquivoUsuarios() {
         return arquivoUsuarios;
     }
 
-    public void setArquivoUsuarios(String arquivoUsuarios) {
-        this.arquivoUsuarios = arquivoUsuarios;
-    }
-
-//    }
     public String getArquivoSeguros() {
         return arquivoSeguros;
-    }
-
-    public void setArquivoSeguros(String arquivoSeguros) {
-        this.arquivoSeguros = arquivoSeguros;
     }
 }
